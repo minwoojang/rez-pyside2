8c5168cacf072c0b1f5765f1dd4f696fb44c26fe
Joshua Senouf <jsenouf@MTLws032.mtloutpost.com>
2019-09-19 19:58:15 -0400

    Misc updates in the Bash script(s).

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 # Will exit the Bash script the moment any command will itself exit with a non-zero status, thus an error.
 set -e
 
-INSTALL_PATH=$1
-PYSIDE2_URL=$2
+INSTALL_PATH=${REZ_BUILD_INSTALL_PATH}
+PYSIDE2_URL=$1
 PYSIDE2_VERSION=${REZ_BUILD_PROJECT_VERSION}
 
-# We print the arguments passed to the Bash script
+# We print the arguments passed to the Bash script.
 echo -e "\n"
 echo -e "==============="
 echo -e "=== INSTALL ==="
@@ -18,14 +18,12 @@ echo -e "[INSTALL][ARGS] INSTALL PATH: ${INSTALL_PATH}"
 echo -e "[INSTALL][ARGS] PYSIDE2 URL: ${PYSIDE2_URL}"
 echo -e "[INSTALL][ARGS] PYSIDE2 VERSION: ${PYSIDE2_VERSION}"
 
-cd ${INSTALL_PATH}
-
-# We install PySide2
+# We install PySide2.
 echo -e "\n"
 echo -e "[INSTALL] Installing PySide2-${PYSIDE2_VERSION}..."
 echo -e "\n"
 
-# We copy the necessary files to the install directory
+# We copy the necessary files to the install directory.
 pip2 install ${PYSIDE2_URL} --target ${INSTALL_PATH} --upgrade --no-dependencies
 
 echo -e "\n"
